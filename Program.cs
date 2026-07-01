d3b5ee2a5261b7268c2f9975df577a6a65467878
Eshraq <eshraq416@gmail.com>
2026-07-01 22:09:35 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1 - Personal Info Card
+            string name = "ESHRAQ";
+            int age = 22;
+            double height = 1.90;
+            bool isStudent = true;
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Height: " + height);
+            Console.WriteLine("Student: " + isStudent);
+
+            Console.WriteLine();
+
+        }
+    }
+}
