001792641974a127c009cee2814bf487d3cbb2bd
Eshraq <eshraq416@gmail.com>
2026-07-01 23:57:02 +0400

    task5

---
+++ task5/task5.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace part1.task5
+{
+    internal class task5
+    {
+
+        static void Main(string[] args)
+        {
+
+            ///// task 5 
+            ///Console.Write("Enter your grade (A, B, C, D, F): ");
+            char grade = Convert.ToChar(Console.ReadLine()!);
+
+            switch (grade)
+            {
+                case 'A':
+                    Console.WriteLine("Excellent");
+                    break;
+                case 'B':
+                    Console.WriteLine("Very Good");
+                    break;
+                case 'C':
+                    Console.WriteLine("Good");
+                    break;
+                case 'D':
+                    Console.WriteLine("Pass");
+                    break;
+                case 'F':
+                    Console.WriteLine("Fail");
+                    break;
+                default:
+                    Console.WriteLine("Invalid grade");
+                    break;
+            }
+        }
+    }
+}
