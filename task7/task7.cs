025fa25a416614d7fbd7f3550f5163da36ae4c0a
Eshraq <eshraq416@gmail.com>
2026-07-02 00:02:54 +0400

    task7

---
+++ task7/task7.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace part1.task7
+{
+    internal class task7
+    {
+
+
+        static void Main(string[] args)
+        {
+            {
+                // Task 7
+                Console.Write("Enter your age: ");
+                int movieAge = Convert.ToInt32(Console.ReadLine());
+
+                string category;
+                double price;
+
+                if (movieAge >= 0 && movieAge <= 12)
+                {
+                    category = "Child";
+                    price = 2.000;
+                }
+                else if (movieAge >= 13 && movieAge <= 59)
+                {
+                    category = "Adult";
+                    price = 5.000;
+                }
+                else
+                {
+                    category = "Senior";
+                    price = 3.000;
+                }
+
+                Console.WriteLine("Category: " + category);
+                Console.WriteLine("Ticket Price: " + price + " OMR");
+
+
+            }
+        }
+    }
+}
