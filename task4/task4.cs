331626992cccea181fd81fdca993b3d228894681
Eshraq <eshraq416@gmail.com>
2026-07-01 23:23:06 +0400

    task4

---
+++ task4/task4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace part1.task4
+{
+    internal class task4
+    {
+
+        static void Main(string[] args)
+        {
+            //task4 
+            Console.Write("Enter your age: ");
+            int voterAge = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Do you have a valid national ID? (yes/no): ");
+            string idAnswer = Console.ReadLine()!;
+            bool hasValidId = (idAnswer == "yes");
+
+            if (voterAge >= 18 && hasValidId)
+            {
+                Console.WriteLine("You are eligible to vote.");
+            }
+            else
+            {
+                Console.WriteLine("You are NOT eligible to vote.");
+            }
+        }
+    }
+}
+        
+    
