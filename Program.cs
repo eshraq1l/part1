bd42440e77092aef6f830eba15947a9bbd6a8ac0
Eshraq <eshraq416@gmail.com>
2026-07-01 22:29:28 +0400

    Task 2 - Rectangle Calculator

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
+        { /*
             //Task 1 - Personal Info Card
             string name = "ESHRAQ";
             int age = 22;
@@ -14,6 +14,22 @@
             Console.WriteLine("Age: " + age);
             Console.WriteLine("Height: " + height);
             Console.WriteLine("Student: " + isStudent);
+
+            Console.WriteLine();
+
+*/
+            //  Task 2 - Rectangle Calculator 
+            Console.Write("Enter rectangle length: ");
+            double length = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter rectangle width: ");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            double area = length * width;
+            double perimeter = 2 * (length + width);
+
+            Console.WriteLine("Area: " + area);
+            Console.WriteLine("Perimeter: " + perimeter);
 
             Console.WriteLine();
 
