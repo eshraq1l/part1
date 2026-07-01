196932cd914d7912a2cf17b1125e61fcf0c35850
Eshraq <eshraq416@gmail.com>
2026-07-01 23:19:28 +0400

    task3

---
+++ task3/task3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace part1.task3
+{
+    internal class task3
+    {
+        static void Main(string[] args)
+        {
+            //task3 
+            Console.Write("Enter a whole number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine(number + " is Even");
+            }
+            else
+            {
+                Console.WriteLine(number + " is Odd");
+            }
+        }
+    }
+}
