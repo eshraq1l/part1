3b65dc2e20182affac44df130867b373320dc3bd
Eshraq <eshraq416@gmail.com>
2026-07-04 19:55:20 +0400

    task13

---
+++ task13/task13.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace part1.task13
+{
+    internal class task13
+    {
+        
+    class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.Write("Enter side 1: ");
+                double a = double.Parse(Console.ReadLine()!);
+
+                Console.Write("Enter side 2: ");
+                double b = double.Parse(Console.ReadLine()!);
+
+                Console.Write("Enter side 3: ");
+                double c = double.Parse(Console.ReadLine()!);
+
+                // triangle inequality - all three combos need to hold
+                bool isValid = (a + b > c) && (a + c > b) && (b + c > a);
+
+                if (!isValid)
+                {
+                    Console.WriteLine("These sides do not form a valid triangle.");
+                }
+                else
+                {
+                    Console.WriteLine("Valid triangle!");
+
+                    if (a == b && b == c)
+                    {
+                        Console.WriteLine("Type: Equilateral");
+                    }
+                    else if (a == b || b == c || a == c)
+                    {
+                        Console.WriteLine("Type: Isosceles");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Type: Scalene");
+                    }
+                }
+            }
+        }
+    }
+}
+
